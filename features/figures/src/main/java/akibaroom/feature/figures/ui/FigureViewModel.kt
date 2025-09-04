fcee144c3c0bb7939c233de2958717a065490647
Juan De la Rosa <juan-delarosa@outlook.com>
2025-09-04 12:54:33 -0600

    Update FigureViewModel.kt

--- features/figures/src/main/java/akibaroom/feature/figures/ui/FigureViewModel.kt
+++ features/figures/src/main/java/akibaroom/feature/figures/ui/FigureViewModel.kt
@@ -15,7 +15,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 internal class FigureViewModel @Inject constructor(
-    private val fetchFigureUseCase: FetchFigureUseCase = FetchFigureUseCase()
+    private val fetchFigureUseCase: FetchFigureUseCase
 ) : MviViewModel<ViewState, ViewEffect, Action>() {
 
     private fun fetchFigures() {
